6a70611d92ee3fd932bec52c93ac76855e831766
Michel Tricot <michel.tricot@gmail.com>
2026-09-01 13:55:21 -0700

    Run console tests deterministically

--- apps/console/vitest.config.ts
+++ apps/console/vitest.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     },
     environment: 'jsdom',
     execArgv: process.allowedNodeEnvironmentFlags.has('--no-experimental-webstorage') ? ['--no-experimental-webstorage'] : [],
-    maxWorkers: 4,
+    maxWorkers: 1,
     setupFiles: ['./src/test/setup.ts'],
     css: false,
   },
